d76922f6dece85f7194f33da57bc337bc5b555d7
Kevin James <KevinJames@thekev.in>
2016-02-11 00:52:08 -0800

    initial package

---
+++ manifests/init.pp
@@ -0,0 +1,42 @@
+class vault ($config, $service_location, $service_template, $url) {
+
+  File { owner => root, group => root }
+
+  exec { 'retrieve_vault':
+    command => "/usr/bin/curl -s ${::vault::url} > /tmp/vault.zip",
+    creates => '/tmp/vault.zip',
+    require => Package['curl'],  # TODO: include these as dependencies
+  } ~>
+  exec { 'unzip_vault':
+    command => '/usr/bin/unzip /tmp/vault.zip -d /opt',
+    creates => '/opt/vault',
+    require => Package['unzip'],  # TODO: include these as dependencies
+  } ~>
+  file { '/opt/vault':
+    mode   => '0755',
+    notify => Service['vault'],
+  }
+
+  file { '/etc/vault.d':
+    ensure  => directory,
+    purge   => true,
+    recurse => true,
+  } ->
+  file { '/etc/vault.d/config.json':
+    content => $::vault::config,  # TODO: consul backend
+                                  # TODO: hiera-ize instead of JSON
+    mode    => '0644',
+    notify  => Service['vault'],
+  }
+
+  file { $::vault::service_location:
+    ensure  => file,
+    content => template($::vault::service_template),  # TODO: check os support
+    mode    => '0644',
+  } ~>
+  service { 'vault':
+    ensure  => running,
+    enable  => true,
+  }
+
+}
