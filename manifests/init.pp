fbe435c836cc5b38f1c32fd53e714a249dc7c64e
Kevin James <KevinJames@thekev.in>
2016-02-23 18:14:44 -0800

    add $tar parameter to base class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-class git($packages, $conflictstyle, $browser, $editor, $mergetool, $pager) {
+class git($packages, $conflictstyle, $browser, $editor, $mergetool, $pager, $tar) {
 
   package { $packages: ensure => latest }
 
