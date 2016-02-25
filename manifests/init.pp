25ee5825bdaf34ab422183c6446be5580076370a
Kevin James <KevinJames@thekev.in>
2016-02-25 00:19:33 -0800

    clean up hiera interpolation

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,8 +2,8 @@ class clementine($packages, $config_file, $config_folder, $config_template, $hom
 
   package { $packages: ensure => latest }
 
-  file { $config_folder: ensure => directory } ->
-  file { $config_file:
+  file { "${home}/${config_folder}": ensure => directory } ->
+  file { "${home}/${config_folder}/${config_file}":
     ensure  => file,
     content => template("clementine/${config_template}"),
     mode    => '0644',
