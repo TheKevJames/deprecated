a5e5fc01590b04a0ba79b036612b767edcc8baba
Kevin James <kevin@aerofs.com>
2016-02-26 13:13:37 -0800

    use XDG_CONFIG_HOME

--- manifests/user.pp
+++ manifests/user.pp
@@ -2,42 +2,43 @@ class git::user($home, $fullname, $email) {
 
   include ::git
 
-  file { "${home}/.gitconfig":
+  file { "${home}/.config/git": ensure => directory } ->
+  file { "${home}/.config/git/templates": ensure => directory } ->
+  file { "${home}/.config/git/templates/hooks":  ensure => directory }
+
+  file { "${home}/.config/git/config":
     ensure  => file,
     content => template('git/config.erb'),
     mode    => '0644',
+    require => File["${home}/.config/git"],
   }
 
-  file { "${home}/.git.d": ensure => directory } ->
-  file { "${home}/.git.d/templates": ensure => directory } ->
-  file { "${home}/.git.d/templates/hooks":  ensure => directory }
-
-  file { "${home}/.git.d/attributes":
+  file { "${home}/.config/git/attributes":
     ensure  => present,
     source  => 'puppet:///modules/git/attributes',
     mode    => '0644',
-    require => File["${home}/.git.d"],
+    require => File["${home}/.config/git"],
   }
 
-  file { "${home}/.git.d/ignore":
+  file { "${home}/.config/git/ignore":
     ensure  => present,
     source  => 'puppet:///modules/git/ignore',
     mode    => '0644',
-    require => File["${home}/.git.d"],
+    require => File["${home}/.config/git"],
   }
 
-  file { "${home}/.git.d/templates/hooks/post-commit":
+  file { "${home}/.config/git/templates/hooks/post-commit":
     ensure  => present,
     source  => 'puppet:///modules/git/postcommit',
     mode    => '0755',
-    require => File["${home}/.git.d/templates/hooks"],
+    require => File["${home}/.config/git/templates/hooks"],
   }
 
-  file { "${home}/.git.d/templates/hooks/pre-commit":
+  file { "${home}/.config/git/templates/hooks/pre-commit":
     ensure  => present,
     source  => 'puppet:///modules/git/precommit',
     mode    => '0755',
-    require => File["${home}/.git.d/templates/hooks"],
+    require => File["${home}/.config/git/templates/hooks"],
   }
 
 }
