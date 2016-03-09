f7e45dc71d6f615014972e5399e73e92ede03451
Kevin James <kevin@aerofs.com>
2016-03-08 16:15:43 -0800

    use xdg config location

--- manifests/user.pp
+++ manifests/user.pp
@@ -2,14 +2,14 @@ class bazaar::user($home, $fullname, $email) {
 
   include ::bazaar
 
-  file { "${home}/.bazaar": ensure => directory } ->
-  file { "${home}/.bazaar/bazaar.conf":
+  file { "${home}/.config/bazaar": ensure => directory } ->
+  file { "${home}/.config/bazaar/bazaar.conf":
     ensure  => file,
     content => template('bazaar/config.erb'),
     mode    => '0644',
   }
 
-  file { "${home}/.bazaar/ignore":
+  file { "${home}/.config/bazaar/ignore":
     ensure  => present,
     source  => 'puppet:///modules/bazaar/ignore',
     mode    => '0644',
