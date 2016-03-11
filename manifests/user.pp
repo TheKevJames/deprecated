416e7d5d3469d1d5e59813b8a27d9bf5ba4c45b2
Kevin James <kevin@aerofs.com>
2016-03-11 11:05:54 -0800

    initial xdg support

--- manifests/user.pp
+++ manifests/user.pp
@@ -2,18 +2,27 @@ class subversion::user($home) {
 
   include ::subversion
 
-  file { "${home}/.subversion": ensure => directory } ->
-  file { "${home}/.subversion/config":
+  file { "${home}/.config/subversion": ensure => directory } ->
+  file { "${home}/.config/subversion/config":
     ensure  => file,
     content => template('subversion/config.erb'),
     mode    => '0644',
   }
 
-  file { "${home}/.subversion/servers":
+  file { "${home}/.config/subversion/servers":
     ensure  => present,
     source  => 'puppet:///modules/subversion/servers',
     mode    => '0644',
-    require => File["${home}/.subversion"],
+    require => File["${home}/.config/subversion"],
+  }
+
+  # subversion does not support XDG. subversion should support XDG.
+  # TODO:
+  #     svn --config-dir "$XDG_CONFIG_HOME"/subversion
+  #     export SUBVERSION_HOME=$XDG_CONFIG_HOME/subversion
+  file { "${home}/.subversion":
+    ensure => link
+    target => "${home}/.config/subversion",
   }
 
 }
