29c5e4cebb8ae803da56cc85a2a5fb9d707bc12d
Kevin James <KevinJames@thekev.in>
2016-04-23 13:17:21 -0700

    update sources before installing packages

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,8 @@
 # Base devbox configuration.
-class devbox($home) {
+class devbox($update_packages_command, $home) {
+
+  exec { 'update-packages': command => $update_packages_command }
+  Exec['update-packages'] -> Package <| |>
 
   ensure_resource(file, ["${home}/.config", "${home}/.config/terminal", "${home}/.config/terminal/extras"], { ensure => directory })
   ensure_resource(file, ["${home}/.local", "${home}/.local/share"], { ensure => directory })
