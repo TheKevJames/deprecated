e6443b055abad65fe9a50da23cb2056abe5944af
Kevin James <KevinJames@thekev.in>
2016-05-16 12:54:02 -0400

    initial package

---
+++ manifests/init.pp
@@ -0,0 +1,34 @@
+# Install and configure phabricator.
+class phabricator($dependencies) {
+
+  require ::git
+
+  include ::devbox::db::mysql
+
+  ensure_packages($dependencies, { ensure => latest })
+
+  vcsrepo { '/opt/arcanist':
+    ensure   => present,
+    source   => 'https://github.com/phacility/arcanist.git',
+    revision => master,
+    provider => git,
+    force    => false,
+  }
+
+  vcsrepo { '/opt/libphutil':
+    ensure   => present,
+    source   => 'https://github.com/phacility/libphutil.git',
+    revision => master,
+    provider => git,
+    force    => false,
+  }
+
+  vcsrepo { '/opt/phabricator':
+    ensure   => present,
+    source   => 'https://github.com/phacility/phabricator.git',
+    revision => master,
+    provider => git,
+    force    => false,
+  }
+
+}
