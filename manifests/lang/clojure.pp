87326140ba7aea3810f5484d827f85307a07600b
Kevin James <kevin@atomicmgmt.com>
2016-08-24 17:47:56 -0700

    install lein to /opt -> /usr/local/bin

--- manifests/lang/clojure.pp
+++ manifests/lang/clojure.pp
@@ -7,14 +7,18 @@ class devbox::lang::clojure($dependencies) {
   ensure_packages($dependencies, { ensure => latest })
 
   exec { 'retrieve_leiningen':
-    command => '/usr/bin/curl -s https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein > /usr/bin/lein',
-    creates => '/usr/bin/lein',
+    command => '/usr/bin/curl -s https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein > /opt/lein',
+    creates => '/opt/lein',
     require => Package[$dependencies],
   } ~>
-  file { '/usr/bin/lein':
+  file { '/opt/lein':
     owner => root,
     group => root,
     mode  => '0755',
+  } ->
+  file { '/usr/local/bin/lein':
+    ensure => link,
+    target => '/opt/lein',
   }
 
 }
